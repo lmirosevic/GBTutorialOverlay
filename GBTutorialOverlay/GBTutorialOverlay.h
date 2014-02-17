80c112a49f267b38ced319507fdf21317f4c5eea
Luka Mirosevic <luka@goonbee.com>
2014-02-17 16:34:57 +0100

    Finished first implementation, untested

--- GBTutorialOverlay/GBTutorialOverlay.h
+++ GBTutorialOverlay/GBTutorialOverlay.h
@@ -10,44 +10,77 @@
 
 #import <GBStickyViews/GBStickyViews.h>
 
+typedef enum {
+    GBTutorialOverlayCloseImagePositionTopRight,
+    GBTutorialOverlayCloseImagePositionBottomRight,
+    GBTutorialOverlayCloseImagePositionBottomLeft,
+    GBTutorialOverlayCloseImagePositionTopLeft,
+} GBTutorialOverlayCloseImagePosition;
+
 @protocol GBTutorialOverlayHintViewInterface;
+@class GBTutorialOverlayHintProperties;
 
-@interface GBTutorialOverlay : UIView
+//class for setting default properties for newly generated overlays
+@interface GBTutorialOverlayDefaults : NSObject
 
-@property (strong, nonatomic) UIColor       *backgroundColor;           //default: black with opacity 0.4
-@property (assign, nonatomic) BOOL          isTapToCloseEnabled;        //default: YES
-@property (assign, nonatomic) BOOL          shouldShowCloseButton;      //defautt: YES
+@property (strong, nonatomic) UIColor                                       *backgroundColor;           //default: black with opacity 0.75
+@property (assign, nonatomic) BOOL                                          isTapToCloseEnabled;        //default: YES
+@property (strong, nonatomic) UIImage                                       *closeButtonImage;          //default: GBTutorialOverlayResources.bundle/GBTutorialOverlayDefaultCloseImage.png
+@property (assign, nonatomic) CGPoint                                       closeButtonOffset;          //default: {10,10}
+@property (assign, nonatomic) GBTutorialOverlayCloseImagePosition           closeButtonPosition;        //default: GBTutorialOverlayCloseImagePositionTopRight
+@property (weak, nonatomic) UIView                                          *viewForPresentation;       //default: [[UIApplication sharedApplication] keyWindow]
 
-+(GBTutorialOverlay *)generateOverlayViewWithHintViewsProperties:(NSArray *)hintViews;//must be an array of GBTutorialOverlayHintProperties objects
--(void)addHintView:(UIView<GBTutorialOverlayHintViewInterface> *)hintView;
+@end
+
+//the actual overlay class
+@interface GBTutorialOverlay : GBTutorialOverlayDefaults
+
+@property (strong, nonatomic, readonly) UIView                              *view;//the view with all the hints on it
+
++(GBTutorialOverlayDefaults *)defaults;
+
++(GBTutorialOverlay *)overlayViewWithStencils:(NSArray *)hintViews;//must be an array of GBTutorialOverlayStencil objects
+-(void)addHintView:(UIView<GBTutorialOverlayHintViewInterface> *)hintView withProperties:(GBTutorialOverlayHintProperties *)hintProperties;
 -(void)present;
 -(void)dismiss;
+-(void)presentAnimated:(BOOL)animated;
+-(void)dismissAnimated:(BOOL)animated;
 
 @end
 
+//properties which encode where and how to display a view
 @interface GBTutorialOverlayHintProperties : NSObject
 
-@property (copy, nonatomic) UIView                      *targetView;
-@property (copy, nonatomic) NSString                    *hintText;
-@property (assign, nonatomic) GBStickyViewsAnchor       masterAnchor;
-@property (assign, nonatomic) GBStickyViewsAnchor       hintAnchor;
-@property (assign, nonatomic) CGPoint                   offset;
-@property (copy, nonatomic) NSString                    *hintViewNibName;
-
-+(GBTutorialOverlayHintProperties *)propertiesWithTargetView:(UIView *)targerView hintText:(NSString *)hintText masterAnchor:(GBStickyViewsAnchor)masterAnchor hintAnchor:(GBStickyViewsAnchor)hintAnchor offset:(CGPoint)offset hintViewNibName:(NSString *)hintViewNibName;
+@property (copy, nonatomic) UIView                                          *targetView;
+@property (assign, nonatomic) GBStickyViewsAnchor                           masterAnchor;
+@property (assign, nonatomic) GBStickyViewsAnchor                           hintAnchor;
+@property (assign, nonatomic) CGPoint                                       offset;
 
 @end
 
+//a stencil for creating views easily
+@interface GBTutorialOverlayStencil : NSObject
+
+@property (strong, nonatomic) GBTutorialOverlayHintProperties               *hintProperties;
+@property (copy, nonatomic) NSString                                        *hintText;
+@property (copy, nonatomic) NSString                                        *hintViewNibName;//the view in this nib at position 0 must conform to GBTutorialOverlayHintViewInterface
+
++(GBTutorialOverlayStencil *)stencilWithNibName:(NSString *)hintViewNibName hintText:(NSString *)hintText targetView:(UIView *)targetView masterAnchor:(GBStickyViewsAnchor)masterAnchor hintAnchor:(GBStickyViewsAnchor)hintAnchor offset:(CGPoint)offset;
+
+@end
+
+//custom hints must conform to this protocol
 @protocol GBTutorialOverlayHintViewInterface <NSObject>
 
-@property (copy, nonatomic) NSString                    *hintText;
+@property (copy, nonatomic) NSString                                        *hintText;
 
 @end
 
+//this is a basic class skeleton for a hint view where the hint text is displayed in a label. Use this as the class in XIB UIView's
 @interface GBTutorialOverlayBasicHintView : UIView <GBTutorialOverlayHintViewInterface>
 
-@property (weak, nonatomic) IBOutlet UILabel            *hintLabel;
+@property (weak, nonatomic) IBOutlet UILabel                                *hintLabel;
 
-@property (copy, nonatomic) NSString                    *hintText;
+@property (copy, nonatomic) NSString                                        *hintText;
 
 @end
--- GBTutorialOverlay/GBTutorialOverlay.m
+++ GBTutorialOverlay/GBTutorialOverlay.m
@@ -8,14 +8,242 @@
 
 #import "GBTutorialOverlay.h"
 
+#import <GBToolbox/GBToolbox.h>
+
+#define kDefaultBackgroundColor                                                 [UIColor colorWithWhite:0 alpha:0.75]
+static BOOL const kDefaultTapToClose =                                          YES;
+#define kDefaultCloseButtonImage                                                [UIImage imageNamed:@"GBTutorialOverlayResources.bundle/GBTutorialOverlayDefaultCloseImage.png"]
+static GBTutorialOverlayCloseImagePosition kDefaultCloseButtonPosition =        GBTutorialOverlayCloseImagePositionTopRight;
+static CGPoint const kDefaultCloseButtonOffset =                                (CGPoint){10, 10};
+#define kDefaultViewForPresentation                                             [[UIApplication sharedApplication] keyWindow]
+
+static NSTimeInterval const kAnimationDuriation =                               0.15;
+
+@interface GBTutorialOverlay ()
+
+@property (strong, nonatomic, readwrite) UIView                                 *view;
+@property (strong, nonatomic) UITapGestureRecognizer                            *tapGestureRecognizer;
+@property (strong, nonatomic) UIButton                                          *closeButton;
+
+@end
+
+@implementation GBTutorialOverlayDefaults
+
+-(id)init {
+    if (self = [super init]) {
+        self.backgroundColor = kDefaultBackgroundColor;
+        self.isTapToCloseEnabled = kDefaultTapToClose;
+        
+        self.closeButtonImage = kDefaultCloseButtonImage;
+        self.closeButtonOffset = kDefaultCloseButtonOffset;
+        self.closeButtonPosition = kDefaultCloseButtonPosition;
+        
+        self.viewForPresentation = kDefaultViewForPresentation;
+    }
+    
+    return self;
+}
+
+@end
+
 @implementation GBTutorialOverlay
 
 #pragma mark - Memory
 
+_singleton(GBTutorialOverlay, defaults)
+
+-(id)init {
+    if (self = [super init]) {
+        //background view
+        self.view = [UIView new];
+        self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_tapHandler:)];
+        [self.view addGestureRecognizer:self.tapGestureRecognizer];
+        
+        //close button (the setters below will configure it)
+        self.closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.closeButton.contentMode = UIViewContentModeCenter;
+        [self.closeButton addTarget:self action:@selector(_closeAction:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:self.closeButton];
+        
+        self.backgroundColor = [self.class defaults].backgroundColor;
+        self.isTapToCloseEnabled = [self.class defaults].isTapToCloseEnabled;
+        self.closeButtonImage = [self.class defaults].closeButtonImage;
+        self.closeButtonOffset = [self.class defaults].closeButtonOffset;
+        self.closeButtonPosition = [self.class defaults].closeButtonPosition;
+        self.viewForPresentation = [self.class defaults].viewForPresentation;
+    }
+    
+    return self;
+}
+
+#pragma mark - CA
+
+-(void)setBackgroundColor:(UIColor *)backgroundColor {
+    super.backgroundColor = backgroundColor;
+    
+    [self _updateBackground];
+}
+
+-(void)setCloseButtonImage:(UIImage *)closeButtonImage {
+    super.closeButtonImage = closeButtonImage;
+    
+    [self _updateCloseButton];
+}
+
+-(void)setCloseButtonOffset:(CGPoint)closeButtonOffset {
+    super.closeButtonOffset = closeButtonOffset;
+    
+    [self _updateCloseButton];
+}
+
+-(void)setCloseButtonPosition:(GBTutorialOverlayCloseImagePosition)closeButtonPosition {
+    super.closeButtonPosition = closeButtonPosition;
+    
+    [self _updateCloseButton];
+}
+
+#pragma mark - API
+
+-(void)addHintView:(UIView<GBTutorialOverlayHintViewInterface> *)hintView withProperties:(GBTutorialOverlayHintProperties *)hintProperties {
+    //add the hint view
+    [self.view addSubview:hintView];
+    
+    //configure the hint view
+    [hintView attachToView:hintProperties.targetView masterAnchor:hintProperties.masterAnchor slaveAnchor:hintProperties.hintAnchor offset:hintProperties.offset track:YES];
+}
+
+-(void)present {
+    [self presentAnimated:YES];
+}
+
+-(void)dismiss {
+    [self dismissAnimated:YES];
+}
+
+-(void)presentAnimated:(BOOL)animated {
+    self.view.frame = self.viewForPresentation.bounds;
+    self.view.alpha = 0.;
+    [self.viewForPresentation addSubview:self.view];
+    
+    [UIView animateWithDuration:kAnimationDuriation animations:^{
+        self.view.alpha = 1.;
+    } completion:^(BOOL finished) {
+        //noop
+    }];
+}
+
+-(void)dismissAnimated:(BOOL)animated {
+    [UIView animateWithDuration:kAnimationDuriation animations:^{
+        self.view.alpha = 0.;
+    } completion:^(BOOL finished) {
+        [self.view removeFromSuperview];
+    }];
+}
+
++(GBTutorialOverlay *)overlayViewWithStencils:(NSArray *)hintViews {
+    GBTutorialOverlay *overlay = [GBTutorialOverlay new];
+    
+    for (GBTutorialOverlayStencil *stencil in hintViews) {
+        if (![stencil isKindOfClass:GBTutorialOverlayStencil.class]) @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Must pass in array of objects with type of GBTutorialOverlayStencil" userInfo:nil];
+        
+        //create the hint view
+        UIView<GBTutorialOverlayHintViewInterface> *hintView = v(stencil.hintViewNibName);
+        hintView.hintText = stencil.hintText;
+        
+        //create the properties object
+        GBTutorialOverlayHintProperties *properties = stencil.hintProperties;
+        
+        //add the hint to the overlay
+        [overlay addHintView:hintView withProperties:properties];
+    }
+    
+    return overlay;
+}
+
+#pragma mark - UITapGestureRecognizer
+
+-(void)_tapHandler:(UITapGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        if (self.isTapToCloseEnabled) {
+            [self dismiss];
+        }
+        else {
+            //noop
+        }
+    }
+}
+
+#pragma mark - UIButton
+
+-(void)_closeAction:(UIButton *)sender {
+    [self dismiss];
+}
+
+#pragma mark - Util
+
+-(void)_updateCloseButton {
+    CGSize buttonSize = self.closeButtonImage.size;
+    CGPoint buttonPosition;
+    UIViewAutoresizing resizingMask;
+    
+    switch (self.closeButtonPosition) {
+        case GBTutorialOverlayCloseImagePositionTopRight: {
+            buttonPosition = CGPointMake(self.view.bounds.origin.x + self.view.bounds.size.width - buttonSize.width - self.closeButtonOffset.x,
+                                         self.view.bounds.origin.y + self.closeButtonOffset.y);
+            resizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin;
+        } break;
+        
+        case GBTutorialOverlayCloseImagePositionBottomRight: {
+            buttonPosition = CGPointMake(self.view.bounds.origin.x + self.view.bounds.size.width - buttonSize.width - self.closeButtonOffset.x,
+                                         self.view.bounds.origin.y + self.view.bounds.size.height - buttonSize.height - self.closeButtonOffset.y);
+            resizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin;
+        } break;
+        
+        case GBTutorialOverlayCloseImagePositionBottomLeft: {
+            buttonPosition = CGPointMake(self.view.bounds.origin.x + self.closeButtonOffset.x,
+                                         self.view.bounds.origin.y + self.view.bounds.size.height - buttonSize.height - self.closeButtonOffset.y);
+            resizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin;
+        } break;
+        
+        case GBTutorialOverlayCloseImagePositionTopLeft: {
+            buttonPosition = CGPointMake(self.view.bounds.origin.x + self.closeButtonOffset.x,
+                                         self.view.bounds.origin.y + self.closeButtonOffset.y);
+            resizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
+        } break;
+    }
+    
+    [self.closeButton setImage:self.closeButtonImage forState:UIControlStateNormal];
+    self.closeButton.frame = CGRectMake(buttonPosition.x, buttonPosition.y, buttonSize.width, buttonSize.height);
+}
+
+-(void)_updateBackground {
+    self.view.backgroundColor = self.backgroundColor;
+}
+
 @end
 
-
 @implementation GBTutorialOverlayHintProperties
+@end
+
+@implementation GBTutorialOverlayStencil
+
++(GBTutorialOverlayStencil *)stencilWithNibName:(NSString *)hintViewNibName hintText:(NSString *)hintText targetView:(UIView *)targetView masterAnchor:(GBStickyViewsAnchor)masterAnchor hintAnchor:(GBStickyViewsAnchor)hintAnchor offset:(CGPoint)offset {
+    GBTutorialOverlayStencil *stencil = [GBTutorialOverlayStencil new];
+
+    stencil.hintText = hintText;
+    stencil.hintViewNibName = hintViewNibName;
+    
+    GBTutorialOverlayHintProperties *hintProperties = [GBTutorialOverlayHintProperties new];
+    hintProperties.targetView = targetView;
+    hintProperties.masterAnchor = masterAnchor;
+    hintProperties.hintAnchor = hintAnchor;
+    hintProperties.offset = offset;
+    stencil.hintProperties = hintProperties;
+    
+    return stencil;
+}
+
 @end
 
 @implementation GBTutorialOverlayBasicHintView
